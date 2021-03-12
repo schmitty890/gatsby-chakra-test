919229c634fa8d523958a34e520a7e97220255f7
Jason Schmitt <schmitty890@gmail.com>
2021-03-12 00:48:37 -0500

    changing to function component instead of class

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,36 @@
 import React, { Component } from "react"
 import { ChakraProvider, Box } from "@chakra-ui/react"
 import {UserProvider, UserConsumer} from "../UserContext"
-import Test from "../components/test/test"
-import TestTwo from "../components/testTwo/testTwo"
-import TestThree from "../components/testThree/testThree"
 import TestHolder from "../components/testHolder/testHolder"
 
-// export default function Home() {
-//   return (
-//     <ChakraProvider>
-//       <UserContext.Provider>
-//         <Box bg="" w="100%" p={4} color="green">
-//           <div>Hello world!</div>
-//           <TestHolder />
-//         </Box>
-//       </UserContext.Provider>
-//     </ChakraProvider>
-//   )
-// }
-
-
-class Home extends Component {
-
-
-  render() {
-    return (
-      <ChakraProvider>
-        <UserProvider>
-            <Box bg="" w="100%" p={4} color="green">
-              <div>Hello world!</div>
-              <TestHolder />
-            </Box>
-        </UserProvider>
-      </ChakraProvider>
-    )
-  }
+export default function Home() {
+  return (
+    <ChakraProvider>
+      <UserProvider>
+          <Box bg="" w="100%" p={4} color="green">
+            <div>Hello world!</div>
+            <TestHolder />
+          </Box>
+      </UserProvider>
+    </ChakraProvider>
+  )
 }
-export default Home
+
+
+// class Home extends Component {
+
+
+//   render() {
+//     return (
+//       <ChakraProvider>
+//         <UserProvider>
+//             <Box bg="" w="100%" p={4} color="green">
+//               <div>Hello world!</div>
+//               <TestHolder />
+//             </Box>
+//         </UserProvider>
+//       </ChakraProvider>
+//     )
+//   }
+// }
+// export default Home
