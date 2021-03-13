b87aa7d2c5fd1e016ca6a4171863f7effc5b92f6
Jason Schmitt <schmitty890@gmail.com>
2021-03-12 22:39:15 -0500

    creating api folder for axios requests

--- src/components/testTwo/testTwo.js
+++ src/components/testTwo/testTwo.js
@@ -1,34 +1,14 @@
 import React, { Component } from "react"
-import axios from "axios"
 import { Button } from "@chakra-ui/react"
 import {UserConsumer} from "../../contexts/UserContext"
 import { get, set } from "../../services/local-storage"
+import { getRandomUser } from "../../api/randomUser"
 class TestTwo extends Component {
   /**
    * React lifecycle method to fetch the data
    */
-  async componentDidMount() {
-
-    try {
-      // fetch data from a url endpoint
-      const response = await axios.get("https://randomuser.me/api/?results=1");
-      console.log(response);
-    } catch (error) {
-      console.log(error); // catches both errors
-    }
-    // axios.get("https://randomuser.me/api/?results=1")
-    //   .then(result => {
-    //     console.log(result)
-    //     // const bookData = result.data
-    //     // this.setState({ bookList: bookData.books })
-    //     // this.rebuildIndex()
-    //   })
-    //   .catch(err => {
-    //     // this.setState({ isError: true })
-    //     console.log("====================================")
-    //     console.log(`Something bad happened while fetching the data\n${err}`)
-    //     console.log("====================================")
-    //   })
+  componentDidMount() {
+    getRandomUser()
   }
 
   // here we are passing the "testInsideComponent function from the main page into a component, and using it in this components function"
