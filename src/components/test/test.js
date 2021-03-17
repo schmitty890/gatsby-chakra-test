fe4761bca86d09d28529d524891ee20403763f5c
Jason Schmitt <schmitty890@gmail.com>
2021-03-16 22:57:13 -0400

    testing chakra ui

--- src/components/test/test.js
+++ src/components/test/test.js
@@ -1,17 +1,29 @@
 import React from "react"
-import { Button, Input, Stack } from "@chakra-ui/react"
+import {
+  Button,
+  Input,
+  Stack,
+  Switch,
+  FormControl,
+  FormLabel,
+  Badge,
+  Flex,
+  Box,
+  Avatar,
+  Text,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react"
 import axios from "axios"
-import {UserConsumer} from "../../contexts/UserContext"
+import { UserConsumer } from "../../contexts/UserContext"
 import myData from "../../data/my-json-content.json"
 
-
-
-
-
 const data = {
-  value1: 'test 1',
-  value2: 'test2'
-};
+  value1: "test 1",
+  value2: "test2",
+}
 
 // function fetchTheData() {
 //   console.log('fetchTheData')
@@ -24,41 +36,98 @@ const data = {
 //     });
 // }
 
-
 async function fetchTheData() {
   try {
     // fetch data from a url endpoint
-    const response = await axios.get("https://randomuser.me/api/?results=1");
-    console.log(response);
+    const response = await axios.get("https://randomuser.me/api/?results=1")
+    console.log(response)
   } catch (error) {
-    console.log(error); // catches both errors
+    console.log(error) // catches both errors
   }
 }
 
 const Test = () => {
   return (
     <UserConsumer>
-      {({user, changeCurrentUser}) => 
+      {({ user, changeCurrentUser }) => (
         <div>
           <div>{user}</div>
-          <Button colorScheme="blue" onClick={changeCurrentUser}>change name to bob</Button>
+          <Button colorScheme="blue" onClick={changeCurrentUser}>
+            change name to bob
+          </Button>
           <h1>{myData.title}</h1>
+          <FormControl display="flex" alignItems="center">
+            <FormLabel htmlFor="email-alerts" mb="0">
+              Enable email alerts?
+            </FormLabel>
+            <Switch id="email-alerts" colorScheme="red" />
+          </FormControl>
+
+          <Stack direction="row">
+            <Badge>Default</Badge>
+            <Badge colorScheme="green">Success</Badge>
+            <Badge colorScheme="red">Removed</Badge>
+            <Badge colorScheme="purple">New</Badge>
+          </Stack>
+
+          <Stack direction="row">
+            <Badge variant="outline" colorScheme="green">
+              Default
+            </Badge>
+            <Badge variant="solid" colorScheme="green">
+              Success
+            </Badge>
+            <Badge variant="subtle" colorScheme="green">
+              Removed
+            </Badge>
+          </Stack>
+
+          <Flex>
+            <Avatar src="https://bit.ly/sage-adebayo" />
+            <Box ml="3">
+              <Text fontWeight="bold">
+                Segun Adebayo
+                <Badge ml="1" colorScheme="green">
+                  New
+                </Badge>
+              </Text>
+              <Text fontSize="sm">UI Engineer</Text>
+            </Box>
+          </Flex>
+
+          <Alert
+            status="success"
+            variant="subtle"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+            height="200px"
+          >
+            <AlertIcon boxSize="40px" mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize="lg">
+              Application submitted!
+            </AlertTitle>
+            <AlertDescription maxWidth="sm">
+              Thanks for submitting your application. Our team will get back to
+              you soon.
+            </AlertDescription>
+          </Alert>
+
           <ul style={{ border: `1px solid red` }}>
             {myData.content.map((data, index) => {
               return <li key={`content_item_${index}`}>{data.item}</li>
             })}
           </ul>
         </div>
-      } 
+      )}
     </UserConsumer>
   )
-};
+}
 
-export default Test;
-
+export default Test
 
 // export default function Test() {
-  
 
 //   return (
 //     <div>
@@ -74,5 +143,3 @@ export default Test;
 //     </div>
 //   )
 // }
-
-
